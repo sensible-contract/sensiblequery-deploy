28fbc7f0b4a91fb5456a5f2f25f74f48a8ce83bd
jiedo <jiedohh@gmail.com>
2021-08-22 17:52:01 +0800

    conf: bitcoin node

---
+++ bitcoin/main-start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+/bitcoin/bitcoin-sv-1.0.8/bin/bitcoind -datadir=/bitcoin/bitcoin-sv-blocks/ -daemon
---
+++ bitcoin/main-stop.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+/bitcoin/bitcoin-sv-1.0.8/bin/bitcoin-cli -datadir=/bitcoin/bitcoin-sv-blocks/ stop
